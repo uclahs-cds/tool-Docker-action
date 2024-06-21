2a228ce7b650345a479a9e4f80aa998285dbdc81
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-06-21 13:19:43 -0700

    Try matching the tag name

--- delete-tags.js
+++ delete-tags.js
@@ -1,5 +1,8 @@
 module.exports = async ({ github, context }) => {
   const { IMAGE_NAME } = process.env
+  const tagName = `branch-${context.payload.ref}`
+
+  let didDelete = false
 
   for await (const response of github.paginate.iterator(
     github.rest.packages.getAllPackageVersionsForPackageOwnedByOrg, {
@@ -7,15 +10,20 @@ module.exports = async ({ github, context }) => {
       package_name: IMAGE_NAME,
       org: context.payload.organization.login
     })) {
-    // let didDelete = false
     for (const version of response.data) {
-      const tags = version.metadata?.container?.tags
-      console.log(version.name)
-      console.log(tags)
-      // didDelete = true
+      if (version.metadata?.container?.tags?.includes?.(tagName)) {
+        console.log(`Package version ${version.html_url} matches tag ${tagName}`)
+        // FIXME Insert actual deletion call here
+        didDelete = true
+        break
+      }
     }
-    // if (didDelete) {
-    //   break
-    // }
+    if (didDelete) {
+      break
+    }
+  }
+
+  if (!didDelete) {
+    console.log(`Failed to find tag named ${tagName}`)
   }
 }
