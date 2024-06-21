9b44900075013b2a40fa32388919d1bcebbb95b1
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-06-21 11:51:43 -0700

    Switch to using semver and ref tags

--- delete-tags.js
+++ delete-tags.js
@@ -1,5 +1,13 @@
 module.exports = ({ github, context }) => {
   console.log('Triggered by a deletion!')
-  console.log(github)
   console.log(context)
+
+  const { IMAGE_NAME } = process.env
+  console.log(IMAGE_NAME)
+
+  // await github.rest.packages.deletePackageVersionForAuthenticatedUser(
+  //   package_type: 'container',
+  //   package_name: IMAGE_NAME,
+  //   package_version_id: ???
+  // )
 }
