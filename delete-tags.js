06dc066c8773366be24890c2ec843a35fbf7241c
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-06-21 12:57:02 -0700

    Search for version

--- delete-tags.js
+++ delete-tags.js
@@ -1,13 +1,13 @@
-module.exports = ({ github, context }) => {
-  console.log('Triggered by a deletion!')
-  console.log(context)
-
+module.exports = async ({ github, context }) => {
   const { IMAGE_NAME } = process.env
-  console.log(IMAGE_NAME)
 
-  // await github.rest.packages.deletePackageVersionForAuthenticatedUser(
-  //   package_type: 'container',
-  //   package_name: IMAGE_NAME,
-  //   package_version_id: ???
-  // )
+  for await (const response of github.paginate.iterator(
+    github.rest.packages.getAllPackageVersionsForAPackageOwnedByAnOrg, {
+      package_type: 'container',
+      package_name: IMAGE_NAME,
+      org: context.payload.organization.login
+    })) {
+    console.log('This is a loop iteration')
+    console.log(response)
+  }
 }
