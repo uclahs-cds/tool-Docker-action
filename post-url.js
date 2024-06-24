d495bb9684d8b01895875392aefbf1ba131b3b62
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-06-24 11:20:53 -0700

    Test using organization input

--- post-url.js
+++ post-url.js
@@ -1,11 +1,11 @@
 module.exports = async ({ github, context, core }) => {
-  const { IMAGE_NAME, IMAGE_DIGEST } = process.env
+  const { ORGANIZATION, IMAGE_NAME, IMAGE_DIGEST } = process.env
 
   for await (const response of github.paginate.iterator(
     github.rest.packages.getAllPackageVersionsForPackageOwnedByOrg, {
       package_type: 'container',
       package_name: IMAGE_NAME,
-      org: context.payload.organization.login
+      org: ORGANIZATION
     })) {
     for (const version of response.data) {
       if (version.name === IMAGE_DIGEST) {
