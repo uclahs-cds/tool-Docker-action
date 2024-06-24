37f8280e080841c9cc8590ea383cc5b425ba433d
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-06-24 09:16:48 -0700

    More bugfix

--- post-url.js
+++ post-url.js
@@ -1,6 +1,8 @@
 module.exports = async ({ github, context, core }) => {
   const { IMAGE_NAME, IMAGE_ID } = process.env
 
+  console.log(`The image name is ${IMAGE_NAME} and the ID is ${IMAGE_ID}`)
+
   for await (const response of github.paginate.iterator(
     github.rest.packages.getAllPackageVersionsForPackageOwnedByOrg, {
       package_type: 'container',
@@ -8,6 +10,8 @@ module.exports = async ({ github, context, core }) => {
       org: context.payload.organization.login
     })) {
     for (const version of response.data) {
+      console.log(`Examining ${version}`)
+
       if (version.name === IMAGE_ID) {
         core.notice(`Uploaded new image ${version.html_url}`)
         return
